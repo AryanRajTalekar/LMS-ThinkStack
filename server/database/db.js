30dacf26d90044da507c4644ba2ac752b186d592
AryanRajTalekar <aryanrajtalekar@gmail.com>
2025-06-24 07:13:13 +0530

    changed db.js

--- server/database/db.js
+++ server/database/db.js
@@ -10,6 +10,7 @@ const connectDB = async () => {
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
   }
 };
 
