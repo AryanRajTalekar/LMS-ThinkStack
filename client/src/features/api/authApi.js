b91259285a484d73efacf3bdd645a04249105676
AryanRajTalekar <aryanrajtalekar@gmail.com>
2025-06-24 07:36:06 +0530

    Removed VITE_BACKEND_URL and used relative API path

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
 
-const USER_API = `${import.meta.env.VITE_BACKEND_URL}/api/v1/user/`;
+const USER_API = `/api/v1/user/`;
 
 
 export const authApi = createApi({
